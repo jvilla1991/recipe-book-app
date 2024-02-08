828f7f89f4edba3b46dadf70ddb7c91042566e60
jvilla1991 <villajeremy1991@gmail.com>
2024-02-07 20:37:12 -0500

    Successfully implmented ability to add Ingredients from Recipe Card to shopping List

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -2,12 +2,11 @@ import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
-
   ingredientChanged = new EventEmitter<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 2),
-    new Ingredient('Tomatoes', 10)
+    new Ingredient('Tomatoes', 10),
   ];
 
   getIngredients(): Ingredient[] {
@@ -18,4 +17,20 @@ export class ShoppingListService {
     this.ingredients.push(ingredient);
     this.ingredientChanged.emit(this.ingredients.slice());
   }
+
+  addIngredients(ingredients: Ingredient[]) {
+    ingredients.forEach((ingredient) => {
+      const ingredientIndex = this.ingredients.findIndex(
+        (existingIngredient) => existingIngredient.name === ingredient.name
+      );
+
+      if (ingredientIndex !== -1) {
+        this.ingredients[ingredientIndex].amount += ingredient.amount;
+      } else {
+        this.ingredients.push(ingredient);
+      }
+    });
+    this.ingredientChanged.emit(this.ingredients.slice());
+  }
+
 }
