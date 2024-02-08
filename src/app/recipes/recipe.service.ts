90c1d94741364acb91a2fd92e9134babd07ec16d
jvilla1991 <villajeremy1991@gmail.com>
2024-02-07 19:27:22 -0500

    Added Ingredients to Recipe Cards

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,11 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipes.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Cajun Shrimp Alfredo', 'This Desciption', 'https://www.nutmegnanny.com/wp-content/uploads/2017/01/cajun-shrimp-fettuccine-alfredo-3.jpg'),
-    new Recipe('Cajun Shrimpie Alfredo', 'Thisie Desciption', 'https://www.nutmegnanny.com/wp-content/uploads/2017/01/cajun-shrimp-fettuccine-alfredo-3.jpg')
+    new Recipe('Cajun Shrimp Alfredo', 'This Desciption', 'https://www.nutmegnanny.com/wp-content/uploads/2017/01/cajun-shrimp-fettuccine-alfredo-3.jpg', [new Ingredient('Shirimp', 3), new Ingredient('Carbs', 300)]),
+    new Recipe('Cajun Shrimpie Alfredo', 'Thisie Desciption', 'https://www.nutmegnanny.com/wp-content/uploads/2017/01/cajun-shrimp-fettuccine-alfredo-3.jpg', [new Ingredient('Shirimp', 2), new Ingredient('Carbs', 0.5)])
   ];
 
   selectedRecipe = new EventEmitter<Recipe>();
